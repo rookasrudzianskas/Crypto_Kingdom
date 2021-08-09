d15975058fe7c5e9bf54073360d42757c61b40ee
Rokas <rookas.rudzenskas@gmail.com>
2021-08-09 17:20:33 +0300

    Rokas

---
+++ src/components/PortfolioCoin/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View} from "../Themed";
+
+const PortfolioCoin = () => {
+    return (
+        <View>
+
+        </View>
+    );
+};
+
+export default PortfolioCoin;
