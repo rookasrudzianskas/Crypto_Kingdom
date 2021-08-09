4c2f6d26af30619b327a1e03ab38cf96fe912be1
Rokas <rookas.rudzenskas@gmail.com>
2021-08-09 14:59:53 +0300

    Rokas

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import {View, Text} from "react-native";
+import {View, Text, Image} from "react-native";
 import tw from 'tailwind-react-native-classnames';
+import styles from "./styles";
+// @ts-ignore
+import image from '../../../assets/images/Saly-1.png';
 
 
 // @ts-ignore
 const HomeScreen = (props) => {
     return (
-        <View style={tw`flex-1 text-2xl font-bold justify-center items-center`}>
+        <View style={styles.root}>
+            <Image source={image} style={styles.image} />
             <Text style={tw` text-2xl font-bold`}>HomeScreen</Text>
         </View>
     );
