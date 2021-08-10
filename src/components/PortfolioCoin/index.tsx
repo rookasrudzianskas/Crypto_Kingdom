4210bf73a823bb32ea46b89365cc2225ed4a0669
Rokas <rookas.rudzenskas@gmail.com>
2021-08-10 12:29:58 +0300

    Rokas

--- src/components/PortfolioCoin/index.tsx
+++ src/components/PortfolioCoin/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {View} from "../Themed";
+import tw from "tailwind-react-native-classnames";
+import styles from "./style";
 
 const PortfolioCoin = () => {
     return (
-        <View>
+        <View style={[styles.root, tw``]}>
 
         </View>
     );
