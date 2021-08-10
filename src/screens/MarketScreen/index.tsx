65d907fe3b08eb692b6647887fc0d00602acbfca
Rokas <rookas.rudzenskas@gmail.com>
2021-08-10 15:43:43 +0300

    Rokas

--- src/screens/MarketScreen/index.tsx
+++ src/screens/MarketScreen/index.tsx
@@ -6,7 +6,7 @@ import image from '../../../assets/images/Saly-17.png';
 import styles from "./styles";
 import PortfolioCoin from "../../components/PortfolioCoin";
 
-const portfolioCoins = [{
+const marketCoins = [{
     id: '1',
     name: 'Virtual Dollars',
     symbol: 'USD',
@@ -48,21 +48,7 @@ const MarketScreen = (props) => {
                 </View>
             </>
             {/*<View style={tw`bg-blu`}>*/}
-            <FlatList showsVerticalScrollIndicator={false} style={{width: '100%'}} data={portfolioCoins} renderItem={({item}) => <PortfolioCoin portfolioCoin={item}  />}
-                //           ListHeaderComponentStyle={{alignItems: 'center'}} ListHeaderComponent={() => (
-                //     <>
-                //         <Image  source={image} style={styles.image}/>
-                //         <View style={[styles.balanceContainer, tw`mb-16`]}>
-                //             <View style={[tw`mt-12`]}>
-                //                 <Text style={tw`text-3xl text-gray-200 font-bold`}>Portfolio balance</Text>
-                //                 <View style={tw`mt-2`}>
-                //                     <Text style={tw`text-3xl text-white font-bold`}>$ <Text style={tw`text-4xl font-extrabold text-green-500 mt-5 text-center`}>64,423</Text></Text>
-                //                 </View>
-                //             </View>
-                //         </View>
-                //     </>
-                // )}
-            />
+            <FlatList showsVerticalScrollIndicator={false} style={{width: '100%'}} data={marketCoins} renderItem={({item}) => <PortfolioCoin portfolioCoin={item}  />}/>
             {/*</View>*/}
         </View>
     );
