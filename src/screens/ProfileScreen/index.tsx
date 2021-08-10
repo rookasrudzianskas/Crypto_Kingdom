f5d385655e552ab4c8207917c5152893c73544b2
Rokas <rookas.rudzenskas@gmail.com>
2021-08-10 18:10:18 +0300

    Rokas

--- src/screens/ProfileScreen/index.tsx
+++ src/screens/ProfileScreen/index.tsx
@@ -26,20 +26,13 @@ const ProfileScreen = (props) => {
                 <Image  source={image} style={[styles.image, tw``]}/>
                 <View style={[styles.balanceContainer, tw`mb-16 items-center`]}>
                    <View>
-                       <View style={[styles.container, tw`items-center bg-blue-700`]}>
-                           <View style={tw`ml-5 flex flex-row bg-blue-700  items-center justify-center`}>
+
                                <Image source={{uri: user.image}} style={[styles.image, tw`rounded-full`]} />
-                           </View>
-                           <View  style={tw`flex-1  bg-blue-700`}>
                                <Text style={[styles.name, tw` text-white ml-5 text-xl font-bold`]}>{user.name}</Text>
                                <Text style={[styles.name, tw` text-white ml-5 text-xl font-bold`]}>{user.email}</Text>
-                           </View>
 
-                           <View style={tw`flex flex-col  bg-blue-700 items-center justify-center`}>
                                <Text style={tw``}>{user.netWorth}</Text>
-                           </View>
 
-                       </View>
                    </View>
                 </View>
             </>
