34c27cf6690b5335f6f65acc295e7a311b58804f
Rokas <rookas.rudzenskas@gmail.com>
2021-08-10 12:26:31 +0300

    Rokas

--- src/screens/PortfolioScreen/index.tsx
+++ src/screens/PortfolioScreen/index.tsx
@@ -14,7 +14,7 @@ const PortfolioScreen = (props) => {
             <View style={styles.balanceContainer}>
                 <View style={[tw`mt-12`]}>
                            <Text style={tw`text-3xl text-gray-200 font-bold`}>Portfolio balance</Text>
-                           <View style={tw`mt-3`}>
+                           <View style={tw`mt-2`}>
                                <Text style={tw`text-3xl text-white font-bold`}>$ <Text style={tw`text-4xl font-extrabold text-green-500 mt-5 text-center`}>64,423</Text></Text>
                            </View>
                        </View>
