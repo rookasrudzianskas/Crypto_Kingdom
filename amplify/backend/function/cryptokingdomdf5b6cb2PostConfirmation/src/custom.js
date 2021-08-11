94a050f0bf078395a50d1bf2c8336b4fe111b5a4
Rokas <rookas.rudzenskas@gmail.com>
2021-08-11 17:12:38 +0300

    Rokas

--- amplify/backend/function/cryptokingdomdf5b6cb2PostConfirmation/src/custom.js
+++ amplify/backend/function/cryptokingdomdf5b6cb2PostConfirmation/src/custom.js
@@ -1,4 +1,37 @@
-exports.handler = (event, context, callback) => {
-  // insert code to be executed by your lambda trigger
-  callback(null, event);
-};
+const aws = require('aws-sdk');
+const ddb = new aws.DynamoDB();
+
+exports.handler = async (event, context) => {
+  if (!event.request.userAttributes.sub) {
+    console.log("Error: No user was written to DynamoDB")
+    context.done(null, event);
+    return;
+  }
+
+  // Save the user to DynamoDB
+  const date = new Date();
+
+  const params = {
+    Item: {
+      'id': { S: event.request.userAttributes.sub },
+      '__typename': { S: 'User' },
+      'email': { S: event.request.userAttributes.email },
+      'name': { S: event.request.userAttributes.name },
+      'image': { S: event.request.userAttributes.image },
+      'createdAt': { S: date.toISOString() },
+      'updatedAt': { S: date.toISOString() },
+    },
+    TableName: process.env.USERTABLE,
+  }
+
+      // give the user 100 000 dollars
+
+  try {
+    await ddb.putItem(params).promise();
+    console.log("Success");
+  } catch (e) {
+    console.log("Error", e);
+  }
+
+  context.done(null, event);
+}
