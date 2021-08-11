914928cda6335233444d81be61849f58057bcc65
Rokas <rookas.rudzenskas@gmail.com>
2021-08-11 10:18:42 +0300

    Rokas

---
+++ src/components/CoinPriceGraph/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from "react-native";
+
+const CoinPriceGraph = () => {
+    return (
+        <View>
+           <Text>Rokas</Text>
+        </View>
+    );
+};
+
+export default CoinPriceGraph;
