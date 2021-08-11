27c7a96c4f2b90db17b521bcf7e4b8af28d57ea2
Rokas <rookas.rudzenskas@gmail.com>
2021-08-11 10:24:49 +0300

    Rokas

--- src/components/CoinPriceGraph/index.tsx
+++ src/components/CoinPriceGraph/index.tsx
@@ -1,10 +1,70 @@
 import React from 'react';
-import {View, Text} from "react-native";
+import {View, Text, Dimensions} from "react-native";
+import {
+    LineChart,
+    BarChart,
+    PieChart,
+    ProgressChart,
+    ContributionGraph,
+    StackedBarChart
+} from "react-native-chart-kit";
 
-const CoinPriceGraph = () => {
+
+interface CoinPriceGraphProps {
+    dataString: string,
+}
+
+const CoinPriceGraph = ({dataString}: CoinPriceGraphProps) => {
+
     return (
         <View>
-           <Text>Rokas</Text>
+            <View>
+                <Text>Bezier Line Chart</Text>
+                <LineChart
+                    data={{
+                        labels: ["January", "February", "March", "April", "May", "June"],
+                        datasets: [
+                            {
+                                data: [
+                                    Math.random() * 100,
+                                    Math.random() * 100,
+                                    Math.random() * 100,
+                                    Math.random() * 100,
+                                    Math.random() * 100,
+                                    Math.random() * 100
+                                ]
+                            }
+                        ]
+                    }}
+                    width={Dimensions.get("window").width} // from react-native
+                    height={220}
+                    yAxisLabel="$"
+                    yAxisSuffix="k"
+                    yAxisInterval={1} // optional, defaults to 1
+                    chartConfig={{
+                        backgroundColor: "#e26a00",
+                        backgroundGradientFrom: "#fb8c00",
+                        backgroundGradientTo: "#ffa726",
+                        decimalPlaces: 2, // optional, defaults to 2dp
+                        color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                        labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                        style: {
+                            borderRadius: 16
+                        },
+                        propsForDots: {
+                            r: "6",
+                            strokeWidth: "2",
+                            stroke: "#ffa726"
+                        }
+                    }}
+                    bezier
+                    style={{
+                        marginVertical: 8,
+                        borderRadius: 16
+                    }}
+                />
+            </View>
+
         </View>
     );
 };
