d04422e3657ac87eff02e33a1c4891a79c57cb41
Rokas <rookas.rudzenskas@gmail.com>
2021-08-11 09:01:59 +0300

    Rokas

--- src/screens/CoinDetailsScreen/index.tsx
+++ src/screens/CoinDetailsScreen/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import {View, Text} from "react-native";
+import {View, Text, Image} from "react-native";
+import styles from "../../components/MarketCoin/style";
+import tw from "tailwind-react-native-classnames";
 
 const CoinDetailsScreen = () => {
     return (
         <View>
-            <Text>ROkas</Text>
+            <View style={[styles.root, tw`items-center bg-blue-700`]}>
+                <View style={tw`ml-5  bg-blue-700`}>
+                    {/*<Image source={{uri: image}} style={[styles.image, tw``]} />*/}
+                </View>
+                <View  style={tw`flex-1  bg-blue-700`}>
+                    <Text style={[styles.name, tw` text-white ml-5 text-xl font-bold`]}>ROkas</Text>
+                    <Text style={[styles.symbol, tw`ml-5  text-white text-sm font-medium`]}>Rokas</Text>
+                </View>
+
+                <View style={tw`flex flex-col  bg-blue-700 items-center justify-center`}>
+
+                </View>
+
+            </View>
         </View>
     );
 };
