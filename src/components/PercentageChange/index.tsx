54886995ffc1e2bc32a7c8b5f473ce2c61e8f528
Rokas <rookas.rudzenskas@gmail.com>
2021-08-11 09:24:47 +0300

    Rokas

--- src/components/PercentageChange/index.tsx
+++ src/components/PercentageChange/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {View, Text} from "react-native";
+import tw from "tailwind-react-native-classnames";
 
-const PercentageChange = () => {
+const PercentageChange = ({value}) => {
     return (
         <View>
-
+            <Text style={[style, {color: value > 0 ? '#4ddb00' : '#f10000'}, tw`font-bold`]}>
+                {value > 0 && '+'} {value}
+            </Text>
         </View>
     );
 };
