0434928b0f040da4576a52d2d558ce6b94c71700
Rokas <rookas.rudzenskas@gmail.com>
2021-08-11 10:52:53 +0300

    Rokas

--- src/screens/CoinExchange/index.tsx
+++ src/screens/CoinExchange/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import {View, Text} from "react-native";
+import {useRoute} from "@react-navigation/native";
 
-const CoinExchangeScreen = () => {
+interface CoinExchangeProps {
+
+}
+
+const CoinExchangeScreen = (props) => {
+
+     const route = useRoute();
+
     return (
         <View>
-           <Text>SOmething cool</Text>
+            <Text>Rokas</Text>
         </View>
     );
 };
